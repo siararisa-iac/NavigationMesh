99c9ffd4c09e96396d6180a0e5e18dc7b4b78979
siararisa-iac <arisa.alcantara@iacademy.edu.ph>
2024-11-09 08:56:30 +0800

    Added NavMeshSample

--- Assets/Scripts/ClickToMove.cs
+++ Assets/Scripts/ClickToMove.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+
 
 public class ClickToMove : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private Transform targetMarker; 
+    private NavMeshAgent agent;
+
+    private void Awake(){
+        agent = GetComponent<NavMeshAgent>();
+    }
+   
+    // Update is called once per frame
+    private void Update()
     {
-        
+        if(Input.GetMouseButtonDown(0))
+        {
+            //Raycast to where the player agent will move
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if(Physics.Raycast(ray, out RaycastHit hitInfo))
+            {
+                UpdateTarget(hitInfo.point);
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void UpdateTarget(Vector3 position)
     {
-        
+        agent.SetDestination(position);
+        targetMarker.position = position;
     }
 }
